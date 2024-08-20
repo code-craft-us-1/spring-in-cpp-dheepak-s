1ff10755a8a006f647bb17a64ed6b0c7ed0bcd0f
S <Dheepak.S@philips.com>
2024-08-20 17:43:47 +0530

    Added the structure and defined the code

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,25 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<double>& data ) {
     //Implement statistics here
+    Stats computedStats;
+    if(data.size() == 0) {
+        computedStats.average = NAN;
+        computedStats.min = NAN;
+        computedStats.max = NAN;
+    } else {
+        double sum = 0;
+        computedStats.min = data[0];
+        computedStats.max = data[0];
+        for(auto i : data) {
+            sum += i;
+            if(i < computedStats.min) {
+                computedStats.min = i;
+            }
+            if(i > computedStats.max) {
+                computedStats.max = i;
+            }
+        }
+        computedStats.average = sum / data.size();
+    }
 }
--- stats.h
+++ stats.h
@@ -1,7 +1,10 @@
 #include <vector>
-
+struct Stats {
+        double average;
+        double min;
+        double max;
+    };
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<double>& data);
 }
